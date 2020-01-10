56a293b659ee0d0e6617d1a2635ca65546ea51a5
mahori <mahori@users.noreply.github.com>
2020-01-11 08:50:28 +0900

    ファイルを追加

---
+++ DBDrives/DBDrives.h
@@ -0,0 +1,32 @@
+﻿
+// DBDrives.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CDBDrivesApp:
+// このクラスの実装については、DBDrives.cpp を参照してください
+//
+
+class CDBDrivesApp : public CWinApp
+{
+public:
+	CDBDrivesApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDBDrivesApp theApp;
